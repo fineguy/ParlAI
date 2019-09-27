13102203c2d22c4723a0c5912574621c2125565f
Timofei Semenov <timasem@replika.ai>
2019-09-27 19:48:34 +0300

    Allow topic generation

--- parlai/tasks/wizard_of_wikipedia/worlds.py
+++ parlai/tasks/wizard_of_wikipedia/worlds.py
@@ -30,6 +30,8 @@ class InteractiveWorld(DialogPartnerWorld):
         self.cnt = 0
         self.human_agent = self.agents[0]
         self.model_agent = self.agents[1]
+        self.topic = None
+        self.topic_list = None
 
     def load_topics(self, opt):
         # Load possible chosen topics
@@ -40,13 +42,15 @@ class InteractiveWorld(DialogPartnerWorld):
         datatype = opt['datatype'].split(':')[0]
         self.topic_list = json.load(open(topics_path, 'rb'))[datatype]
 
-    def get_new_topic(self):
+    def generate_topics(self):
         random.seed()
         topics = random.sample(self.topic_list, self.num_topics - 1)
         topics.append(NO_TOPIC)
         letters = list(string.ascii_uppercase)[: self.num_topics]
-        topic_list = {x: y for x, y in zip(letters, topics)}
-        topic_text = '\n'.join(['{}: {}'.format(k, v) for k, v in topic_list.items()])
+        self.topic_list = {x: y for x, y in zip(letters, topics)}
+
+    def get_new_topic(self):
+        topic_text = '\n'.join(['{}: {}'.format(k, v) for k, v in self.topic_list.items()])
 
         done = False
         while not done:
@@ -58,20 +62,23 @@ class InteractiveWorld(DialogPartnerWorld):
             )
             topic_act = self.human_agent.act()
             choice = topic_act['text'][0].upper()
-            if choice in topic_list:
+            if choice in self.topic_list:
                 done = True
             else:
                 self.human_agent.observe(
                     {'text': 'Invalid response, please try again.'}
                 )
 
-        chosen_topic = topic_list[choice]
+        chosen_topic = self.topic_list[choice]
         print('[ Your chosen topic is: {} ]'.format(chosen_topic))
         return chosen_topic
 
     def parley(self):
-        if self.cnt == 0:
+        if self.cnt == 0 and not self.topic_list:
+            self.generate_topics()
             self.topic = self.get_new_topic()
+
+        if self.cnt == 0:
             self.acts = [None, None]
             self.human_first = random.choice([0, 1])
 
